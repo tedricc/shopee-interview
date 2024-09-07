786afbf8cbc8c7c76edb38e17502279364200d78
Tedric <chow.tedric@gmail.com>
2024-09-07 22:30:35 +0800

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,45 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import MainImage from "@/public/main.jpg";
+import Icon from "@/public/icon.jpg";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className={styles.home}>
+      <div className={styles.top}>
+        <div className={styles.top__info}>
+          Seller Centre | Start Selling | Download | Follow us on Facebook and
+          Instagram
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className={styles.top__info}>
+          Notifications | Help | English | Tedric Chow
+        </div>
+      </div>
+
+      <div className={styles.favs__header}>
+        <div className={styles.favs__container}>
+          <div className={styles.favs__logo}>
+            <Image src={Icon} />
+            Shopee | Favourite Items
+          </div>
+
+          <div className={styles.favs__search}>
+            Search for products and shops
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.main}>
+        <div className={styles.main__heading}>
+          <h1>Your Lists</h1>
+
+          <div>Create a List | Get List Help</div>
+        </div>
+        <div className={styles.image__container}>
+          <Image src={MainImage} />
+        </div>
+      </div>
     </div>
   );
 }
